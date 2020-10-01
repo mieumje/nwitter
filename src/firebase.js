07874d9c5220f822a40962c596278f3921d7b310
mieumje <66072832+mieumje@users.noreply.github.com>
2020-10-01 19:57:38 +0900

    #1.1 Securing the Keys
    환경변수 선언

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDF0Ng0hTNfGG1BhCE_uYT_ECr2aFE3xuw",
-  authDomain: "nwitter-f9118.firebaseapp.com",
-  databaseURL: "https://nwitter-f9118.firebaseio.com",
-  projectId: "nwitter-f9118",
-  storageBucket: "nwitter-f9118.appspot.com",
-  messagingSenderId: "665073545718",
-  appId: "1:665073545718:web:384247bc1058d9e1a457d7",
+  apiKey: process.env.REAT_APP_API_KEY,
+  authDomain: process.env.REAT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REAT_APP_DATABASE_URL,
+  projectId: process.env.REAT_APP_PROJECT_ID,
+  storageBucket: process.env.REAT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REAT_APP_MESSAGIN_ID,
+  appId: process.env.REAT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);

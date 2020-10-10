de33d8ce93607a87b39ed75dd83364f71e1ac536
mieumje <66072832+mieumje@users.noreply.github.com>
2020-10-10 16:49:09 +0900

    #3.5 Delete and Update part Two
    delete

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,15 +1,25 @@
+import { dbService } from "fbase";
 import React from "react";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete</button>
-        <button>Edit</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?");
+    console.log(ok);
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button onClick={onDeleteClick}>Delete</button>
+          <button>Edit</button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Nweet;
